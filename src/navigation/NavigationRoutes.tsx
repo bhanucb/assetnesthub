ff548084006ac2d85d2010343ce268a0a0385167
Bhanu <7218584+bhanucb@users.noreply.github.com>
2024-01-10 14:40:56 -0500

    Update NavigationRoutes.tsx

--- src/navigation/NavigationRoutes.tsx
+++ src/navigation/NavigationRoutes.tsx
@@ -1,8 +1,7 @@
 import { Outlet, Route, Routes } from "react-router-dom";
-import NavigationBar from "./NavigationBar";
 import Home from "../pages/home/Home";
-import Popout from "../pages/popout/Popout";
 import NotFound from "../pages/starter/NotFound";
+import NavigationBar from "./NavigationBar";
 
 export type IpaRoute = {
   path: string;
@@ -30,7 +29,8 @@ function NavigationRoutes() {
         <Route path="/" element={<Home />} />
       </Route>
       <Route path="/test/:tabId" element={<div>TEST</div>} />
-      <Route path="/popout/:tabId" element={<Popout />} />
+      {/* <Route path="/popout/:tabId" element={<Popout />} /> */}
+      <Route path="/popout/:tabId" element={<div>TEST</div>} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
