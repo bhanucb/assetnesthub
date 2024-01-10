035b4dd1d9391916d8d21dabb8ab22f73695ff08
Bhanu <7218584+bhanucb@users.noreply.github.com>
2024-01-10 15:29:03 -0500

    Add NavigationBar and Popout components to NavigationRoutes

--- src/navigation/NavigationRoutes.tsx
+++ src/navigation/NavigationRoutes.tsx
@@ -1,7 +1,8 @@
 import { Outlet, Route, Routes } from "react-router-dom";
-import Home from "../pages/home/Home";
-import NotFound from "../pages/starter/NotFound";
 import NavigationBar from "./NavigationBar";
+import Home from "../pages/home/Home";
+import Popout from "../pages/popout/Popout";
+import NotFound from "../pages/starter/NotFound";
 
 export type IpaRoute = {
   path: string;
@@ -28,9 +29,9 @@ function NavigationRoutes() {
       <Route element={<PageWithNavigationBar />}>
         <Route path="/" element={<Home />} />
       </Route>
-      <Route path="/test/:tabId" element={<div>TEST</div>} />
+      <Route path="/test/:tabId" element={<Popout />} />
       {/* <Route path="/popout/:tabId" element={<Popout />} /> */}
-      <Route path="/popout/:tabId" element={<div>TEST</div>} />
+      {/* <Route path="/popout/:tabId" element={<Popout />} /> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
