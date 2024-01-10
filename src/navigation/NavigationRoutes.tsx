39a35425ff6d46ad81eaf70ca00a543586dd035a
Bhanu <7218584+bhanucb@users.noreply.github.com>
2024-01-10 14:09:47 -0500

    Update test route path in NavigationRoutes.tsx

--- src/navigation/NavigationRoutes.tsx
+++ src/navigation/NavigationRoutes.tsx
@@ -11,7 +11,7 @@ export type IpaRoute = {
 
 export const NAVBAR_LINKS: Array<IpaRoute> = [
   { path: "/", name: "Home" },
-  { path: "/test", name: "Test" },
+  { path: "/test/something", name: "Test" },
 ];
 
 function PageWithNavigationBar() {
@@ -29,7 +29,7 @@ function NavigationRoutes() {
       <Route element={<PageWithNavigationBar />}>
         <Route path="/" element={<Home />} />
       </Route>
-      <Route path="/test" element={<div>TEST</div>} />
+      <Route path="/test/:id" element={<div>TEST</div>} />
       <Route path="/popout/:tabId" element={<Popout />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
